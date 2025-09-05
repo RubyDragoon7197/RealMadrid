dad6cee088ef5725e10c694136a7362c106db13f
RubyDragoon7197 <Andrespedrozab2004@gmail.com>
2025-09-05 05:41:42 +0000

    cambio_3

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const anuncios = [
+    {
+      id: 1,
+      imagen: "https://tpc.googlesyndication.com/simgad/3294518566630187960"
+    },
+    {
+      id: 2,
+      imagen: "https://s0.2mdn.net/dfp/1761334/5356108316/1749194940185/Banner_Dubai_May_728x90/728x90_IMG03.jpg"
+    },
+    {
+      id: 3,
+      imagen: "https://tpc.googlesyndication.com/simgad/834068855906244193"
+    }
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="lista-anuncios">
+      {anuncios.map((item) => (
+        <img
+          key={item.id}
+          src={item.imagen}
+          alt={`anuncio-${item.id}`}
+          className="banner"
+        />
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
